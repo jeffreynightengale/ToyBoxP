f4c6500db8cd0d3f696ab984b2a41b90dfc1efc5
Jeff <Jeff@DESKTOP-DFBAMBV>
2021-08-02 15:16:21 -0500

    started

---
+++ ToyBox/ToyBox/Toy.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToyBox
+{
+    class Toy
+    {
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        private string Notes;
+        public Toy()
+        {
+            Manufacturer = string.Empty;
+            Name = string.Empty;
+            Price = 0;
+        }
+        public Toy(string notes)
+        {
+            Notes = notes;
+        }
+        public void GettingNotes(string note)
+        {
+            Notes += note;
+        }
+        public string ReturningNotes()
+        {
+            return Notes;
+        }
+        public string GetAisle()
+        {
+            Random rand = new Random();
+            int randomNum = rand.Next(1, 25);
+
+            string aisleNumber = $"{Manufacturer[0]}{randomNum}";
+            return aisleNumber;
+        }
+    }
+}
---
+++ ToyBox/ToyBox/ToyBox.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToyBox
+{
+    class ToyBox
+    {
+        public List<string> Toys { get; set; }
+        public string Owner { get; set; }
+        public string Location { get; set; }
+        public ToyBox()
+        {
+            Toys = new List<string>();
+            Owner = string.Empty;
+            Location = string.Empty;
+        }
+        
+    }
+}
